242d0cf862e75d32bae1604fd8a57933b8850881
claudiu <ygdrax@protonmail.com>
2025-08-14 20:35:48 +0300

    Added backend deployment

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import psycopg2
+import os
+
 
 app = FastAPI()
 
@@ -12,8 +14,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-    
-app
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    return conn
 
 @app.get("/")
 def read_root():
@@ -21,13 +37,7 @@ def read_root():
 
 @app.get("/users")
 def get_users():
-    conn = psycopg2.connect(
-        dbname="fullstack",
-        user="fullstack",
-        password="fullstack",
-        host="db",
-        port="5432"
-    )
+    conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users")
     users = cursor.fetchall()
@@ -37,13 +47,7 @@ def get_users():
 
 @app.post("/users")
 def create_user(user_id: int, name: str):
-    conn = psycopg2.connect(
-        dbname="fullstack",
-        user="fullstack",
-        password="fullstack",
-        host="db",
-        port="5432"
-    )
+    conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("INSERT INTO users (id, name) VALUES (%s, %s) RETURNING id", (user_id, name))
     user_id = cursor.fetchone()[0]
@@ -54,13 +58,7 @@ def create_user(user_id: int, name: str):
 
 @app.delete("/users/{user_id}")
 def delete_user(user_id: int):
-    conn = psycopg2.connect(
-        dbname="fullstack",
-        user="fullstack",
-        password="fullstack",
-        host="db",
-        port="5432"
-    )
+    conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("DELETE FROM users WHERE id = %s", (user_id,))
     conn.commit()
@@ -69,4 +67,4 @@ def delete_user(user_id: int):
     return {"message": "User deleted successfully"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
